0483eaad2617911315eee7f64be1c7dd9d8314f1
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2021-02-19 22:35:36 +0300

    Added routing

--- src/components/DoctorsList/DoctorsList.jsx
+++ src/components/DoctorsList/DoctorsList.jsx
@@ -1,12 +1,16 @@
 import React, {Fragment, PureComponent} from "react";
 import './DoctorsList.css'
+import {Link} from "react-router-dom";
+
 
 class DoctorsList extends PureComponent {
 
     render() {
         const list = [{
+            id: 1,
             name: "User1"
         }, {
+            id: 2,
             name: "User2"
         }];
 
@@ -14,15 +18,17 @@ class DoctorsList extends PureComponent {
             <Fragment>
                 <div style={{backgroundColor: 'rgba(256, 0, 0, 0.2)'}}>
                     {list.map(el => {
-                        return <p style={{width: '200px'}} onClick={this.onRecordClick(el)}>{el.name}</p>
+                        return <div>
+                            <Link exact to={"/schedule/" + el.id}>
+                                <p style={{width: '200px'}} onClick={() => {
+                                    console.log(el.name)
+                                }}>{el.name}</p>
+                            </Link>
+                        </div>
                     })}
                 </div>
             </Fragment>
         );
-    }
-
-    onRecordClick(element) {
-        console.log(element.name)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/Schedule/Schedule';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import DoctorsList from './components/DoctorsList/DoctorsList';
 import store from './store';
+import Schedule from "./components/Schedule/Schedule";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <DoctorsList />
+            <Router>
+                <Switch>
+                    <Route path="/schedule/:id" component={Schedule}/>
+                    <Route path="/" component={DoctorsList}/>
+                </Switch>
+            </Router>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
